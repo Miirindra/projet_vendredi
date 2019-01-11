4be6d6237f0993065fe464576962194c6b3feb4b
Hery Mirindra <hmirindra@gmail.com>
2019-01-11 16:33:47 +0300

    first commit

---
+++ lib/journalists.rb
@@ -0,0 +1,107 @@
+#NB: @ definir un var globale
+
+@tab = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+#Nombre des journalistes dans ce array
+def nombre_journalist
+	puts "il y a #{@tab.length} journalistes dans ce array"	
+end
+
+#handle contiennent un numéro
+def handle_avec_numero
+	t = Array.new
+	t = @tab.select {|v| v =~ /[0123456789]/ }
+	puts "le handle contiennent de numero sont #{t.length} "
+end
+
+#handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?
+def handle_four_lettre
+	t = @tab.grep(/[Aa]ude/) 
+	puts t.inspect
+end
+
+#Combien commencent par une majuscule (première lettre juste après le @)
+
+def commence_par_maj
+	maj = Array.new
+	maj = @tab.select {|y| y =~/@[A-Z]/ }
+	puts "Les nombres de majuscule contenu dans le handle sont : #{maj.length}"
+end
+
+#Combien y a-t-il de underscore _ dans tous les pseudos confondus
+def contains_underscore
+	underscore = 0
+	for n in @tab
+		underscore = underscore + n.scan(/_/).length
+	end
+	puts "Les nombres des underscore sont : #{underscore}"
+end
+
+#Trie la liste de handle par ordre alphabétique
+def tri
+	ordre = @tab.sort_by {|alpha| alpha.downcase}
+	puts ordre.inspect
+	puts ordre.size
+end
+
+#Quelle est la position dans l'array de la personne @epenser
+def position
+	puts "@epenser se trouve au #{@tab.index('@epenser')}é position"
+end
+
+#Combien contiennent une majuscule
+def contains_maj
+	maj = 0
+	for n in @tab
+		if n.scan(/[A-Z]/).length == 1
+			maj = maj + 1
+		end
+	end
+	puts "il y a #{maj} majuscule dans le tableau" 
+end
+
+#Quels sont les 50 handles les plus courts de ce array
+def fifty_handle_short
+	val = @tab.sort_by(&:length)
+	tab = val[0..49].inspect
+	puts "les 50 handles les plus courts de ce array sont : \n #{tab}"
+end
+
+def menu
+	puts "Entrer votre choix?"
+	puts "1.Nombre des journalistes dans ce array" 
+	puts "2.Nombre d'handle contiennent un numéro"
+	puts "3.Nombre d'handle contiennent les 4 lettres du prénom 'Aude' à la suite"
+	puts "4.Nombre d'handle commencent par une majuscule (première lettre juste après le @)"
+	puts "5.Nombre d'handle contiennent une majuscule" 
+	puts "6.Nombre underscore _ dans tous les pseudos confondus"
+	puts "7.Trie la liste de handle par ordre alphabétique"
+	puts "8.les 50 handles les plus courts de ce array"
+	puts "9.la position dans l'array de la personne @epenserputs"
+	print ">> "
+
+	choix = gets.chomp.to_i
+
+	if choix == 1
+		nombre_journalist
+	elsif choix == 2
+		handle_avec_numero
+	elsif choix == 3
+		handle_four_lettre
+	elsif choix == 4
+		commence_par_maj
+	elsif choix == 5
+		contains_maj
+	elsif choix == 6
+		contains_underscore
+	elseif choix == 7
+		tri
+	elsif choix == 8
+		fifty_handle_short
+	elsif choix == 9
+		position
+	else  puts "Erreur, veuillez choisir un de ces chiffres!"
+	end
+end
+
+menu #permet a vos utilisateur de choisir ce qu'ils veulent savoir
